48d20b6b8c4f20fd136ad8390130c33c665448cd
Ndeta Innocent <you@example.com>
2022-07-22 13:37:21 +0300

    making a color and point with struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+mod rectangles;
 fn main() {
     println!("Hello, world!");
-
     struct User {
         username: String,
         email: String,
@@ -14,4 +14,20 @@ fn main() {
         sign_in_count: 1,
     };
     println!("user's email is {}", user1.email);
+    fn build_email(email: String, username: String) -> User {
+        User {
+            username,
+            email,
+            sign_in_count: 1,
+            active: true,
+        }
+    }
+    rectangles::area(34, 23);
+}
+
+fn tupple_struct() {
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
---
+++ src/rectangles.rs
@@ -0,0 +1,8 @@
+pub fn area(height: i32, width1: i32) -> i32 {
+    height * width1
+}
+
+pub struct rectangle {
+    width: u32,
+    height: u32,
+}
